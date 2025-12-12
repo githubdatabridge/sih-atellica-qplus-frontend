bf468abfc3f017513ab9a571d462ae077eded7ae
Slobodan <slobodan.mikaric@codebihind.rs>
2025-12-12 09:00:05 +0100

    chore: update certs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,25 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import dns from "dns";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import fs from "fs";
-// import mkcert from "vite-plugin-mkcert";
 
 dns.setDefaultResultOrder("verbatim");
 
-export default defineConfig({
-    base: "/",
+export default defineConfig(({ mode }) => {
+  // Load environment variables for the current mode
+  // The third argument ('') is the prefix; an empty string loads all variables.
+  // By default, it only loads variables prefixed with 'VITE_'
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+   base: "/",
     plugins: [react(), tsconfigPaths()],
     build: {
         outDir: "build"
     },
     server: {
-        port: 7001,
+        port: env.PORT ? parseInt(env.PORT) : 7000,
         open: true,
-        host: "localhost",
+        host: env.HOST || "localhost",
         https: {
-            key: fs.readFileSync("./cert/localhost.key"),
-            cert: fs.readFileSync("./cert/localhost.crt")
+            key: fs.readFileSync(env.SSL_KEY_FILE || "../certificates/server/server.key"),
+            cert: fs.readFileSync(env.SSL_CRT_FILE || "../certificates/server/server.crt")
         }
     },
     define: {
@@ -32,5 +37,5 @@ export default defineConfig({
           '@mui/material/Tooltip'
         ],
       },
-    
+  };
 });
